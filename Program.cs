334ac35a809906f3c570e6c56d6c56bf6902c2f2
Rimsha Fayyaz <rimshafayyaz13@gmail.com>
2021-11-24 21:06:49 +0500

    commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int start = 0, end = 0;
+            Console.WriteLine("start = 0, end = 0");
+            Console.ReadLine();
+        }
+    }
+}
